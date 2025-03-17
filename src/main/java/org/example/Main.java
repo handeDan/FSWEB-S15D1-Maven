0645b86f709fb8df2f7fda916bc951c46d88c5c6
hande <hande.dann@gmail.com>
2025-03-17 15:19:27 +0300

    Lists & ArrayLists

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,63 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("***********");
+
+        List<Contact> myContacts = new ArrayList<>();
+        MobilePhone myPhone = new MobilePhone("123456789", myContacts);
+
+        Contact alice = Contact.createContact("Alice", "555-1234");
+        Contact bob = Contact.createContact("Bob", "555-5678");
+
+        myPhone.addNewContact(alice);
+        myPhone.addNewContact(bob);
+
+        myPhone.printContact();
+
+        Contact newAlice = Contact.createContact("Alice", "555-9999");
+        myPhone.updateContact(alice, newAlice);
+
+        myPhone.printContact();
+
+        myPhone.removeContact(bob);
+        myPhone.printContact();
+
+        System.out.println("***********");
+
+        List<Contact> hisContacts = new ArrayList<>();
+        MobilePhone hisPhone = new MobilePhone("000-000-00-00", hisContacts);
+
+        Contact jerry = Contact.createContact("Jerry", "11235813");
+        Contact matthew = Contact.createContact("Matthew", "23571113");
+
+        System.out.println(jerry.getPhoneNumber().equals(matthew.getPhoneNumber()));
+        System.out.println(jerry.getName() + "'s phone number is: " + jerry.getPhoneNumber());
+        System.out.println(matthew.getName() + "'s phone number is: " + matthew.getPhoneNumber());
+
+        hisPhone.addNewContact(jerry);
+        hisPhone.addNewContact(matthew);
+        hisPhone.addNewContact(jerry);
+
+        hisPhone.printContact();
+
+        Contact result = hisPhone.queryContact("matthew");
+
+        if (result != null) {
+            System.out.println("Bulunan Contact: " + result.getName() + " - " + result.getPhoneNumber());
+        } else {
+            System.out.println("Contact bulunamadı.");
+        }
+
+        System.out.println("***********");
 
     }
 }
